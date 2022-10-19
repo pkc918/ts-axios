ac9b5e21375d5daf88e8c5ce2cc791d968b2364d
XiaoPan <2489964425@qq.com>
2022-10-19 23:11:43 +0800

    examples

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export type METHOD =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+export interface AxiosRequestConfig {
+  url: string
+  method?: METHOD
+  data?: any
+  params?: any
+}
