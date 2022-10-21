f1b4005f7780499e6c4b79d5d3a3556a8944dbb7
XiaoPan <2489964425@qq.com>
2022-10-21 22:35:16 +0800

    headers 实例

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -75,3 +75,25 @@ axios({
   url: '/base/buffer',
   data: new Int32Array([21, 31])
 })
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json',
+    Accept: 'application/json, text/plain, */*'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const paramString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramString)
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+})
