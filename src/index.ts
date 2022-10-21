8df1143dd29042651b7046832264d27ed106de53
XiaoPan <2489964425@qq.com>
2022-10-21 20:34:59 +0800

    参数在 url 中拼接

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import { buildURL } from './helpers/url'
 import { AxiosRequestConfig, METHOD } from './types'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
-  // TODO
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
